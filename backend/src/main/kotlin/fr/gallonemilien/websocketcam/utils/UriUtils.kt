35d1ab3ee983ac7df9848fa45308432180a4bbcf
GallonEmilien <gallonemilien@gmail.com>
2025-09-10 17:45:15 +0200

    fix: query in getQueryParams could be null

--- backend/src/main/kotlin/fr/gallonemilien/websocketcam/utils/UriUtils.kt
+++ backend/src/main/kotlin/fr/gallonemilien/websocketcam/utils/UriUtils.kt
@@ -4,11 +4,15 @@ import java.net.URI
 
 object UriUtils {
     fun getQueryParams(uri: URI?): Map<String, String> {
-        if (uri?.query.isNullOrEmpty()) return emptyMap()
-        return uri.query.split("&")
+        val query = uri?.query ?: return emptyMap()
+        return query.split("&")
             .mapNotNull {
-                val parts = it.split("=")
-                if (parts.isNotEmpty()) parts[0] to (parts.getOrNull(1) ?: "") else null
+                val parts = it.split("=", limit = 2)
+                if (parts.isNotEmpty()) {
+                    parts[0] to (parts.getOrNull(1) ?: "")
+                } else {
+                    null
+                }
             }
             .toMap()
     }
